a4832a4deea1bac9f0731b96d2f75b06b7be5367
bilhares <felipe.bilhares@hotmail.com>
2020-05-19 15:53:56 -0400

    reactive form professor

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Professor } from '../models/professor';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-professores',
@@ -8,8 +9,9 @@ import { Professor } from '../models/professor';
 })
 export class ProfessoresComponent implements OnInit {
 
-  professorSelecionado : Professor;
+  professorSelecionado: Professor;
   titulo = "Professores"
+  professorForm: FormGroup;
 
   public professores = [
     { id: 1, nome: 'Adalberto', disciplina: 'Matematica' },
@@ -18,17 +20,30 @@ export class ProfessoresComponent implements OnInit {
     { id: 4, nome: 'Licio', disciplina: 'Geografia' },
   ]
 
-  selecionarProfessor(professor:Professor){
-    this.professorSelecionado = professor;
+  constructor(private fb: FormBuilder) {
+    this.criarForm();
   }
-
-  voltar(){
-    this.professorSelecionado = null;
-  }
-
-  constructor() { }
 
   ngOnInit(): void {
   }
 
+  selecionarProfessor(professor: Professor) {
+    this.professorSelecionado = professor;
+    this.professorForm.patchValue(professor);
+  }
+
+  voltar() {
+    this.professorSelecionado = null;
+  }
+
+  criarForm() {
+    this.professorForm = this.fb.group({
+      nome: ['', Validators.required],
+      disciplina: ['', Validators.required]
+    });
+  }
+
+  professorSubmit() {
+    console.log(this.professorForm.value);    
+  }
 }
